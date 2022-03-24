347f72983d20286f73709db1cd3e7ba4bc89c1f9
Gommez <alexandre.gommez@edu.ece.fr>
2022-03-24 21:38:25 +0100

    Fichier problème

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -2,15 +2,47 @@ from sklearn.base import BaseEstimator
 from sklearn import ensemble
 import sklearn
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+import category_encoders as ce
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.model = ensemble.RandomForestRegressor(max_depth=40, n_estimators=100, random_state=57, n_jobs=-1)
+        self.ohe_cols = ['classement', 'region', 'commentaire', 'type', 'consommation', 'elevage']
+        self.tgt_cols = ['vin', 'producteur', 'appellation']
+        ohe_pipeline = Pipeline([
+        ("one-hot-encoder", OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        tgt_pipeline = Pipeline([
+            ("target-encoder", ce.TargetEncoder())
+        ])
+
+        preprocessing = ColumnTransformer([
+            ("ohe_preproc", ohe_pipeline, self.ohe_cols),
+            ("tgt_preproc", tgt_pipeline, self.tgt_cols)
+        ])
+
+        self.model = Pipeline([
+            ("Preprocessing", preprocessing),
+            ("regressor", LinearRegression())
+        ])
 
     def fit(self, X, Y):
-        self.model.fit(np.log(X), np.log(Y))
+        print(X.info())
+        X.drop(columns=['guide',  'garde', 'cuvee',
+                        'prod_id',
+                        'adresse', 'CP', 'commune', 'pays',
+                        'INSEE_COM', 'INSEE_DEP', 'INSEE_REG'], inplace=True)
+        self.model.fit(X, Y)
+        
 
     def predict(self, X):
-        res = self.model.predict(np.log(X))
-        return np.exp(res)
+        X.drop(columns=['guide',  'garde', 'cuvee',
+                        'prod_id',
+                        'adresse', 'CP', 'commune', 'pays',
+                        'INSEE_COM', 'INSEE_DEP', 'INSEE_REG'], inplace=True)
+        return self.model.predict(X)
 
